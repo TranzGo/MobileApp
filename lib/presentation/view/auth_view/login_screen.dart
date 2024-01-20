143da52f10ae094426eaa95fe25c61058a1e88fc
iammrjude <judeabara@gmail.com>
2024-01-20 11:20:42 +0100

    login screen

--- lib/presentation/view/auth_view/login_screen.dart
+++ lib/presentation/view/auth_view/login_screen.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tranzgoo/presentation/view/auth_view/components/my_text_field.dart';
+import 'package:tranzgoo/utils/theme/app_colors.dart';
+import 'package:tranzgoo/utils/theme/app_style.dart';
+import 'package:tranzgoo/utils/widget/app_button.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,8 +14,118 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final passwordController = TextEditingController();
+  final emailController = TextEditingController();
+  IconData iconPassword = CupertinoIcons.eye_fill;
+  bool obscurePassword = true;
+  String? _errorMsg;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/tranzgooLogo.png',
+                color: AppColors.primaryColor,
+                height: 80.08.h,
+                width: 140.w,
+              ),
+              Text(
+                'Login',
+                style: AppText.extraBold.copyWith(
+                  fontFamily: 'LeagueSpartan',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 19.sp,
+                  color: AppColors.blackColor,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: MyTextField(
+                  controller: emailController,
+                  hintText: 'Email',
+                  obscureText: false,
+                  keyboardType: TextInputType.emailAddress,
+                  prefixIcon: const Icon(CupertinoIcons.mail_solid),
+                  errorMsg: _errorMsg,
+                  validator: (val) {
+                    if (val!.isEmpty) {
+                      return 'Please fill in this field';
+                    } else if (!RegExp(r'^[\w-\.]+@([\w-]+.)+[\w-]{2,4}$')
+                        .hasMatch(val)) {
+                      return 'Please enter a valid email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: MyTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: obscurePassword,
+                  keyboardType: TextInputType.visiblePassword,
+                  prefixIcon: const Icon(CupertinoIcons.lock_fill),
+                  errorMsg: _errorMsg,
+                  validator: (val) {
+                    if (val!.isEmpty) {
+                      return 'Please fill in this field';
+                    } else if (!RegExp(
+                            r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~`)\%\-(_+=;:,.<>/?"[{\]}\|^]).{8,}$')
+                        .hasMatch(val)) {
+                      return 'Please enter a valid password';
+                    }
+                    return null;
+                  },
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        obscurePassword = !obscurePassword;
+                        if (obscurePassword) {
+                          iconPassword = CupertinoIcons.eye_fill;
+                        } else {
+                          iconPassword = CupertinoIcons.eye_slash_fill;
+                        }
+                      });
+                    },
+                    icon: Icon(iconPassword),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              AppButton(
+                onPressed: () {},
+                label: 'Login',
+                isText: true,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.fingerprint,
+                  size: 50.sp,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
